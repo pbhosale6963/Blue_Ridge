18b292761dc2ae4702be232541097d92b948b368
pbhosale6963 <pbhosale6963@gmail.com>
2022-05-22 16:47:39 +0530

    Project Completed

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoDBURL = "mongodb://localhost:27017/BlueRidge"
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
